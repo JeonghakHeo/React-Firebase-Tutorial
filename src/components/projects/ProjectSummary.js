2208e6f5b318aeca861d2a9e067481ed3f883e98
Charlie <huh4142@naver.com>
2020-09-12 21:05:48 +1000

    Project Summary Component

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ProjectList from './ProjectList'
+
+// keep the template of ProjectList, making the app more modular
+const ProjectSummary = () => {
+  return(
+    <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Projcet Title</span>
+          <p>Posted by the Net Ninja</p>
+          <p className="grey-text">3rd September, 2am</p>
+        </div>
+      </div>
+  )
+}
+
+export default ProjectSummary
