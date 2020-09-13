481bdfc334af768cc69d4aa28b7b96d50a1ab4a1
Charlie <huh4142@naver.com>
2020-09-13 22:09:47 +1000

    Login & Signup Components

---
+++ src/components/auth/SignIn.js
@@ -0,0 +1,45 @@
+// this has to be class-based component 
+// because we want to store, in a local store, what a user types in to the input field
+
+// rce to create this template
+import React, { Component } from 'react'
+  
+class SignIn extends Component {
+  state = {
+    email: '',
+    password: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      // to indetify which property to update
+      [e.target.id]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state)
+  }
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSubmit}className="white">
+          <h5 className="grey-text text-darken-3">Sign In</h5>
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" onChange={this.handleChange}/>
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" onChange={this.handleChange}/>
+          </div>
+          <div className="input-field">
+            <button className="btn pink lighten-1 z-depth-0">LOGIN</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SignIn
