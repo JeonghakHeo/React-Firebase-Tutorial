ee439d26f4cca7f2939c2c2b5a55cde64d8adb53
Charlie <huh4142@naver.com>
2020-09-15 20:09:51 +1000

    lesson-14 Creating a Firebase Project

---
+++ src/config/fbConfig.js
@@ -0,0 +1,20 @@
+// npm install firebase
+// importing just base features from firebase library by adding /app
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+var config = {
+  apiKey: "AIzaSyBiOzcREFKfwEpnSlZfujbLKIs4kLRUHzI",
+  authDomain: "net-ninja-marioplan-6c444.firebaseapp.com",
+  databaseURL: "https://net-ninja-marioplan-6c444.firebaseio.com",
+  projectId: "net-ninja-marioplan-6c444",
+  storageBucket: "net-ninja-marioplan-6c444.appspot.com",
+  messagingSenderId: "483252353244",
+  appId: "1:483252353244:web:e3fc6aa6927da072955c19"
+};
+
+firebase.initalizeApp(config);
+firebase.firestore().settings({ timestmpsInSnapshots: true })
+
+export default fireabse;
