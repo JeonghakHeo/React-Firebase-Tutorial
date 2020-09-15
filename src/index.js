dd5acc0db5d262182fdcf552cc08fba26334e477
Charlie <huh4142@naver.com>
2020-09-15 20:37:04 +1000

    lesson-16 Connecting Redux to Firebase

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 // create store in index.js
 // import applyMiddlewarfe from redux for thunk
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
 
 // allows our application to have access to reducer
@@ -25,7 +25,21 @@ import thunk from 'redux-thunk'
 // we can have many enhancers inside createstore(rootReducer, A, B, C...)
 // they enhance store with extra functionalities 
 // with functionalities, we can return a function inside our action creators then interact with database
-const store = createStore(rootReducer, applyMiddleware(thunk));
+
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+import fbConfig from './config/fbConfig'
+
+// we want to use both getFirestore and getFirebase so we can access the firebase
+// or firestore API inside the function in projectAction.js
+const store = createStore(rootReducer, 
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    // tells where to connect to
+    reduxFirestore(fbConfig),
+    reactReduxFirebase(fbConfig)
+  )
+);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 serviceWorker.unregister();
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -6,9 +6,13 @@ export const createProject = (project) => {
   // above is normal way
 
   // with thunk we can return a function
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async call to database
     // we can pause dispatch and then we can caryy on with dispatch
     dispatch({ type: 'CREATE_PROJECT', project: project });
   }
 };
+
+// npm install react-redux-firebase redux-firestore
+// react-redux-firebase provides bidings to the firebase service as a whole
+// redux-firestore provides redux bindings for firestore databases
