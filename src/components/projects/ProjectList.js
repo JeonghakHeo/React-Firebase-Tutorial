20ee341037ef41514f1b56b3d05eba7e4efde6bb
Charlie <huh4142@naver.com>
2020-09-18 09:55:22 +1000

    lesson-20 Project Details Data

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ProjectSummary from './ProjectSummary'
-
+import { Link } from 'react-router-dom'
 // receiving those props by doing ({projects})
 const ProjectList = ({projects}) => {
   
@@ -14,7 +14,9 @@ const ProjectList = ({projects}) => {
       
       { projects && projects.map(project => {
         return(
+          <Link to={'/project/' + project.id}>
           <ProjectSummary project={project} key={project.id} />
+          </Link>
         )
       })}
     </div>
