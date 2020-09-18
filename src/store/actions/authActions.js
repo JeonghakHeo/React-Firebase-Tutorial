3464e0917ea696997147b75a51316ff4d610c590
Charlie <huh4142@naver.com>
2020-09-18 21:46:00 +1000

    lesson-23 Logging Users In

---
+++ src/store/actions/authActions.js
@@ -0,0 +1,17 @@
+// credentials are going to be email and password
+export const signIn = (credentials) => {
+  // because of thunk we can halt the process and return a function 
+  return (dispatch, getState, {getFirebase}) => {
+    const firebase = getFirebase();
+
+    firebase.auth().signInWithEmailAndPassword(
+      credentials.email,
+      credentials.password
+    ).then(() => {
+      dispatch({ type: 'LOGIN_SUCCESS' })
+    }).catch((err) => {
+      dispatch({ type: 'LOGIN_ERROR', err })
+    });
+
+  }
+}
