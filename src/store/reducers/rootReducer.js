0965f56da2386d5dfc48be2c2e2665b9e5fb0d67
Charlie <huh4142@naver.com>
2020-09-18 21:09:21 +1000

    lesson-21 Firebase Auth with Redux

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -7,10 +7,14 @@ import { combineReducers } from 'redux'
 // a premade reducer that is made for syncing our firestore data with our state
 import { firestoreReducer } from 'redux-firestore'
 
+// for firebase authentication 
+import { firebaseReducer } from 'react-redux-firebase'
+
 const rootReducer = combineReducers({
   auth: authReducer,
   project: projectReducer,
-  firestore: firestoreReducer
+  firestore: firestoreReducer,
+  firebase: firebaseReducer
 });
 
 export default rootReducer
