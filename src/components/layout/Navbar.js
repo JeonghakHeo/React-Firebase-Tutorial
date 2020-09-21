01942458b10f38f68a03b6c77778939877b148db
Charlie <huh4142@naver.com>
2020-09-21 14:35:06 +1000

    lesson-25 Tracking Auth Status

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,22 +5,27 @@ import SignedOutLinks from './SignedOutLinks'
 // we are just connecting to our reducers not firestore.
 // thus just connect, not firestoreConnect
 import { connect } from 'react-redux'
-const Navbar = () => {
+
+const Navbar = (props) => {
+  // grabbing the auth property we attached down from auth: state;firebase.auth
+  const { auth } = props;
+  // console.log(auth)
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
   return(
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to='/' className="brand-logo">MarioPlan</Link>
-        <SignedInLinks />
-        <SignedOutLinks />
+        { links }
       </div>
     </nav>
   )
 }
 
 const mapStateToProps = (state) => {
-  console.log(state)
+  // console.log(state)
   return {
-
+    // check out console.log(state).
+    auth: state.firebase.auth
   }
 }
 
