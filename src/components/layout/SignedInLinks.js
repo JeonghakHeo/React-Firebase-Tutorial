90a60378e031712aba46dab94499496a8c4db6bb
Charlie <huh4142@naver.com>
2020-09-21 14:03:42 +1000

    Logging Users Out

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 // to get 'active' class and show when only signed in
-const SignedInLinks = () => {
+
+import { connect } from 'react-redux'
+import { signOut } from '../../store/actions/authActions'
+
+// this is not a functional component so we need to take props
+// so we can't this.props 
+const SignedInLinks = (props) => {
+
   return(
     <ul className="right">
       <li><NavLink to='/create'>New Project</NavLink></li>
-      <li><NavLink to='/'>Log Out</NavLink></li>
+      {/* if it was onClick={props.signOut() the function will run automatically when the page loads} */}
+      <li><a onClick={props.signOut}>Log Out</a></li>
       <li><NavLink to='/' className='btn btn-floating pink lighten-1'>NN</NavLink></li>
     </ul>
   )
 }
 
-export default SignedInLinks
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signOut: () => dispatch(signOut())
+  }
+}
+
+export default connect(null, mapDispatchToProps)(SignedInLinks)
