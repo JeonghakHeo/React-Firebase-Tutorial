4d9584c76c8b47b95b67eb9592d9b26c11535151
Charlie <huh4142@naver.com>
2020-09-22 18:23:04 +1000

    lesson-35 Notification Component

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,13 @@
 // sudo npm install -g firebase-tools
 // set up cloud function, refer to lesson-33 
+
+// this funciton will run on firebase server, not in the browser
 const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  // functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello ninjas!");
+});
