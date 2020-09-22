810f765000132b927e4a80d470d29bb6ce9a038c
Charlie <huh4142@naver.com>
2020-09-22 15:48:48 +1000

    lesson-33 Cloud Functions Intro & Setup

---
+++ functions/index.js
@@ -0,0 +1,11 @@
+// sudo npm install -g firebase-tools
+// set up cloud function, refer to lesson-33 
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
