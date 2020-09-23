b13b86324e6099f6a5150545dda2346e2cd37fd7
Charlie <huh4142@naver.com>
2020-09-23 11:02:42 +1000

    lesson-36 Cloud Function Firestore Trigger

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,36 @@
 // sudo npm install -g firebase-tools
 // set up cloud function, refer to lesson-33 
 
+
+
 // this funciton will run on firebase server, not in the browser
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+// we will be able to use admin SDK to interact with
+// different services like authentication or firestore service
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase)
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  // functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello ninjas!");
+});
+
+const createNotification = (notification) => {
+  return admin.firestore().collection('notifications')
+  .add(notification)
+  .then(doc => console.log('notification added', doc))
+}
+
+exports.projectCreated = functions.firestore
+  .document('projects/{projectId}')
+  .onCreate(doc => {
+
+    const project = doc.data();
+    const notification = {
+      content: 'Added a new project',
+      user: `${project.authorFirstName} ${project.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    }
+
+    return createNotification(notification);
 });
