111f9f07ca7b5989eccafc2ce3cf1c748e15f152
Charlie <huh4142@naver.com>
2020-09-23 12:19:14 +1000

    lesson-40 Deploying to Firebase Hosting

---
+++ dist/precache-manifest.849a315cbc7fe9a85e66bfaf3a40a88c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4395b7347cf9ba0a9713e58379c0b4f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85d8b72abda5e6629156",
+    "url": "/static/css/main.5ec5607c.chunk.css"
+  },
+  {
+    "revision": "a3ae3cbb772aab2ec121",
+    "url": "/static/js/2.cb02d986.chunk.js"
+  },
+  {
+    "revision": "d56f7d70e49a582c3cb88e84fa36367e",
+    "url": "/static/js/2.cb02d986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85d8b72abda5e6629156",
+    "url": "/static/js/main.fd5fde88.chunk.js"
+  },
+  {
+    "revision": "acdf868312f78a97cdd0",
+    "url": "/static/js/runtime-main.794c9aa6.js"
+  }
+]);
